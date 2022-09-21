f175a233fa7dfdb73853270c553f9b4fe5cebea4
Yusuf Bozkurt YETİŞGEN <91032065+Yusuf-Yetisgen@users.noreply.github.com>
2022-09-21 20:59:51 +0300

    Add files via upload

---
+++ sayiBulma.java
@@ -0,0 +1,21 @@
+public class sayiBulma {
+
+    public static void main (String[] args){
+        int[] sayilar = new int [] {1,2,5,9,0};;
+        int aranacak = 5;
+        boolean varMi=false;
+
+        for (int sayi : sayilar) {
+            if (sayi==aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+
+        if (varMi){
+            System.out.println("Sayı mevcut");
+        }else {
+            System.out.println("Sayı Mevcut Değil");
+        }
+    }
+}
